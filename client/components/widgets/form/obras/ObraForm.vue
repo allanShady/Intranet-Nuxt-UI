4218b49a17098549ff06adb4246f723cfa864767
Guimaraes Mahota <guimaraesmahota@gmail.CO.MZ>
2019-11-22 09:25:31 +0200

    barcode reader

--- client/components/widgets/form/obras/ObraForm.vue
+++ client/components/widgets/form/obras/ObraForm.vue
@@ -1,22 +1,30 @@
 <template>
   <v-form v-model="valid">
-    <v-container grid-list-xl fluid>
+    <v-container grid-list-xl fluid id="watch-example">
       <v-layout row wrap>
         <v-flex lg6>
-          <div id="intro" style="text-align:center;">
-            <h1>{{ formModel.timestamp }}</h1>
-          </div>
+          
+          <v-text-field  type="text" id ="barcode" v-model="scan1"
+            label="Codigo de Barras"
+             @input="someHandler" 
+             @change="someHandler" 
+             v-on:keypress="keymonitor"
+             ></v-text-field> 
 
+          <input  type="text" id ="funcBarcode" v-model="funcScan" hidden="hidden"/>
+
+          <input  type="text" id ="projetoBarcode" v-model="projetoScan" hidden="hidden"/>
 
           <v-select
+            id= "listFuncionario"
             :items="funcionarios"
             v-validate="'required'"
-            v-if="funcionarios.CodBarras === filter || filter === ''"
+            v-if="funcionarios.Codigo === filter || filter === ''"
             data-vv-name="funcionario"
             :error-messages="errors.collect('funcionario')"
             v-model="formModel.funcionario"
             label="Nome do Fúncionario"
-            auto
+            
             required
             item-text="Nome"
             item-value="Codigo"
@@ -26,13 +34,14 @@
           </v-select>
 
           <v-select
+            id= "listProjetos"
             :items="projetos"
             v-validate="'required'"
             data-vv-name="projeto"
             :error-messages="errors.collect('projeto')"
             v-model="formModel.projeto"
             label="Projeto"
-            auto
+            
             required
             item-text="Descricao"
             item-value="Projeto"
@@ -60,6 +69,10 @@
 </template>
 
 <script>
+
+import Funcionarios from '@/api/obra/funcionarios';
+import Projetos from '@/api/obra/projetos';
+
 export default {
   props: ['filter'],
 
@@ -67,6 +80,10 @@ export default {
     validator: "new"
   },
   data: () => ({
+    el: '#watch-example',
+    scan1: "",
+    funcScan: "",
+    projetoScan: "",
     formModel: {
       country: null,
       date: new Date().toISOString().substr(0, 10),
@@ -84,127 +101,109 @@ export default {
       timestamp: "",
       filter:''
     },
-    funcionarios: [
-      {
-        Codigo: "001",
-        Nome: "Maria João Pereira Alves",
-        CodBarras: "1600108501844"
-      },
-      {
-        Codigo: "A001",
-        Nome: "Filomena Maria Cruz Rebelo",
-        CodBarras: "1600102521459"
-      },
-      {
-        Codigo: "A002",
-        Nome: "João Carlos Araújo Fonseca",
-        CodBarras: "1600103807354"
-      },
-      {
-        Codigo: "A003",
-        Nome: "Carla Alexandra Gomes Azevedo",
-        CodBarras: "1600104700111"
-      },
-      {
-        Codigo: "A017",
-        Nome: "Filipe Manuel Marques da Silva Dias",
-        CodBarras: "1600105200160"
-      },
-      {
-        Codigo: "B001",
-        Nome: "Luís Augusto Pinto Teixeira Neves",
-        CodBarras: "1600105201013"
-      },
-      {
-        Codigo: "D002",
-        Nome: "Sandra Regina Queirós Azevedo Conceição",
-        CodBarras: "1600152010132"
-      },
-      {
-        Codigo: "F001",
-        Nome: "Ana Cristina Leite Casaco",
-        CodBarras: "1600107010330"
-      },
-      {
-        Codigo: "F002",
-        Nome: "Olga Maria Casal Melo da Cunha",
-        CodBarras: "1600108501844"
-      },
-      {
-        Codigo: "F003",
-        Nome: "Ana Alexandra Valença Pinto",
-        CodBarras: "1600108511324"
-      }
-    ],
-    projetos: [
-      {
-        Projeto: "11",
-        Descricao: "Centros de custo",
-        CodBarras: "8850585992458"
-      },
-      { Projeto: "11001", Descricao: "Sede", CodBarras: "8850585992526" },
-      {
-        Projeto: "11002",
-        Descricao: "Filial Av. da Liberdade",
-        CodBarras: "8850585992632"
-      },
-      {
-        Projeto: "11003",
-        Descricao: "Filial Av. Central",
-        CodBarras: "8850585992649"
-      }
-    ],
+    funcionarios: Funcionarios,
+    projetos: Projetos,
 
     valid: true
   }),
   created() {
     setInterval(this.getNow, 1000);
-
+    
+    
     
   },
+watch: {
+
+  // sempre que a pergunta mudar, essa função será executada
+    scan1: function (scan1New,scan1Old) {
+
+      var pressed = false; 
+      var chars = [];
+      
+      if (pressed == false) {
+
+        var barcode = scan1New;
+        
+        if(barcode.length >  10){
+
+            var func = Funcionarios.filter(d => d.CodBarras === barcode);
+            
+            if(func.length > 0){
+
+              this.funcScan = func[0].Codigo;
+              this.scan1="";   
+              
+            }
+
+            var proj = Projetos.filter(d => d.CodBarras === barcode);
+            
+            if(proj.length > 0){
+
+              this.projetoScan = proj[0].Projeto;
+              this.scan1="";   
+              
+            }
+            
+        }else{
+
+        }
+        chars = [];
+        
+      }      
+    },
+    // sempre que a pergunta mudar, essa função será executada
+    funcScan: function () {
+
+      this.formModel.funcionario =this.funcScan; 
+      this.scan1="";   
+    },
+    // sempre que a pergunta mudar, essa função será executada
+    projetoScan: function () {
+      this.formModel.projeto =this.projetoScan; 
+      this.scan1="";   
+    }
+  },
+
   mounted() {
     this.$validator.localize("en", this.dictionary);
 
-    var pressed = false; pressed = true;
+    var pressed = false; 
     var chars = [];
 
-    window.addEventListener("keypress", function(e) {
+    /* window.addEventListener("keypress", function(e) {
       if (e.which >= 48 && e.which <= 57) {
         chars.push(String.fromCharCode(e.which));
       }
-      //console.log(e.which + ":" + chars.join("|"));
+
       if (pressed == false) {
    
         setTimeout(function() {
           if (chars.length >= 10) {
             var barcode = chars.join("");
             console.log("Barcode Scanned: " + barcode);
-            // assign value to some input (or do whatever you want)
-            //$("#MainContent_txtInput").val(barcode);
-            //var selected = $('#ListBoxfuncionario').find('option:selected').text();
-            //var selected = $("#MainContent_ListBoxfuncionario")
-            //  .find("option:selected")
-            //  .val();
             
-            if (selected == "") {
-              getFuncionarioCodigoBarras();
-            } else {
-              var btn = document.getElementById("MainContent_NewFerramentaBt");
-              if (btn) btn.click();
+              var func = Funcionarios.filter(d => d.CodBarras === barcode);
+            
+            if(func.length > 0){
+              //this.funcBarcode.value = func[0].Codigo;
+              
+              document.querySelector("#funcBarcode").innerHTML = func[0].Codigo;
+              //this.$forceUpdate();
+
             }
           } else {
               
           }
           chars = [];
-          pressed = false;
+
+          
+          //pressed = false;
         }, 500);
-        pressed = true;
-      }else{
-        this.filter = "1600107010330";
-        console.log(this.filter);
+        //pressed = true;
       }
       
-    });
+    }); */
+    
   },
   methods: {
     submit() {
@@ -214,6 +213,16 @@ export default {
       this.formModel = {};
       this.$validator.reset();
     },
+    someHandler: function(evt){
+      
+    },
+    keymonitor: function(e) {
+
+    }
+  },
+  computed: {
+  location: () => window.location,
+  
     getNow: function() {
       const today = new Date();
       const date =
@@ -227,9 +236,6 @@ export default {
       const dateTime = date + " " + time;
       this.timestamp = dateTime;
     }
-  },
-  computed: {
-  location: () => window.location,
 }
 };
 </script>
