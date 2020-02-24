7f2b83b63a0951e11bbcccbe067d3dfa7e7e3da0
Alan Camilo <acamilo.ag@gmail.com>
2020-02-24 18:42:29 +0200

    setup of Vue Store

--- client/store/index.js
+++ client/store/index.js
@@ -10,4 +10,40 @@ export const mutations = {
     state.drawer = val
   }
 }
+
+export const actions = {
+  authenticateUser(vuexContext, user) {
+    console.log('vue context', vuexContext);
+    return this.$axios.$post('/auth/login', {
+      email: user.email,
+      password: user.password
+  })
+  .then(result => { 
+    console.log('THE USER TOKEN: ', result.token)
+    
+    vuexContext.commit("setToken", result.token);
+    localStorage.setItem("token", result.token);
+    localStorage.setItem(
+      "tokenExpiration",
+      new Date().getTime() + result.expiresIn * 1000
+    );
+
+    vuexContext.dispatch("setLogoutTimer", result.expiresIn * 1000);
+  })
+  .catch(error => {console.log(error)}); 
+  },
+
+  async getDataAsync(vuexContext, resource) {
+    return await this.$axios
+     .$get(`/${resource}`)
+     .then(response => response)
+     .catch(error => console.log(error));
+  },
+
+  setLogoutTimer(vuexContext, duration) {
+    setTimeout(() => {
+      vuexContext.commit("clearToken");
+    }, duration);
+  },
+} 
 //mongodb+srv://mahotag:<password>@cluster0-5qt2r.mongodb.net/test?retryWrites=true&w=majority
