8c2eed04f232e4250521ccca1965f09199fff766
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-02-25 17:13:05 +0200

    add document dashboard

--- client/components/inv/dashboard.vue
+++ client/components/inv/dashboard.vue
@@ -1,19 +1,32 @@
 <template>
-  <v-card></v-card>
+  <v-layout>
+    <v-row>
+      <v-btn-toggle>
+        <v-bottom-navigation
+    :value="activeBtn"
+    color="primary"
+    horizontal>
+          <v-btn v-for="document in documentTypes" :key="document">
+              <span>{{document.code}}</span>
+              <v-icon>{{document.icon}}</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+      </v-btn-toggle>
+    </v-row>
+  </v-layout>
 </template>
 <script>
-
 export default {
-
   data: () => ({
     documentTypes: []
   }),
   beforeMount: async function() {
     let doc = this.$router.currentRoute.query["id"];
-    console.log(doc);
-    this.documentTypes = await this.$store.dispatch("getDataAsync", 'documenttypes/' +doc);
 
-    console.log(this.documentTypes);
+    this.documentTypes = await this.$store.dispatch(
+      "getDataAsync",
+      "documenttypes/" + doc
+    );
   }
 };
 </script>
