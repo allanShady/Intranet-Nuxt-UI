be4cdb33cdd3fbd6306d8ce32716404ae7b500c2
Alan Camilo <acamilo.ag@gmail.com>
2020-02-27 15:40:04 +0200

    Removes comments and debug logs

--- client/middleware/custom-auth.js
+++ client/middleware/custom-auth.js
@@ -1,11 +1,6 @@
 export default function(context) {
-    console.log("[Middleware] Just Auth");
-    console.log(context.store)
-
     const expirationDate = new Date(localStorage.getItem('expiration_date'))
     
     if (!(localStorage.getItem('token') && (new Date() < expirationDate)))
         context.redirect("/login")
-
-        //Call store and clear auth data
 }
