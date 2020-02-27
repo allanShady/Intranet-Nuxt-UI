b9735ef3a242ad3245176e989167688b772bb2f3
Alan Camilo <acamilo.ag@gmail.com>
2020-02-27 14:45:27 +0200

    Added a way to get token from local storage

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,6 +1,5 @@
 export default function ({ $axios, redirect }, inject) {
   // Create a custom axios instance
-  const token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwiZW1haWwiOiJhbGFuLmNhbWlsb0BpbmNlbnRlYS5jb20iLCJuYmYiOjE1ODIyNzg2NTYsImV4cCI6MTU4Mjg4MzQ1NiwiaWF0IjoxNTgyMjc4NjU2fQ.BsWy2DF8BE7qz_aoKyt5OvTfjBSqC95XTSyQoREFC0gs9ZRgnpVeuudHuyRgTo3H09Mo6hsqeVIcgdM80DsjZQ';
   const axios = $axios.create({
     headers: {
       common: {
@@ -13,7 +12,7 @@ export default function ({ $axios, redirect }, inject) {
   axios.setBaseURL(process.env.baseUrl);
 
   axios.onRequest((config) => {
-    config.headers.common['Authorization'] = `Bearer ${token}`;
+    config.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
   });
 
   // Inject to context as $axios
