e38a4f7d37632bcf5c9a2b4a27da09e7aae99ffd
Allan Camilo <acamilo.ag@gmail.com>
2020-03-19 10:38:33 +0200

    layout fix footer message

--- client/layouts/dashboard.vue
+++ client/layouts/dashboard.vue
@@ -13,9 +13,9 @@
           <!-- App Footer -->
           <v-footer height="auto" class="white pa-3 app--footer">
             <span class="caption">&copy; {{ new Date().getFullYear() }}</span>
-            <v-spacer></v-spacer>
-            <span class="caption mr-1">Made with love</span>
-            <v-icon color="pink" small>favorite</v-icon>
+            <!--v-spacer></v-spacer-->
+            <!--span class="caption mr-1">Made with love</span>
+            <v-icon color="pink" small>favorite</v-icon-->
           </v-footer>
         </v-content>
         <!-- Go to top -->
@@ -65,6 +65,7 @@ import PageHeader from '@/components/PageHeader'
 import ThemeSettings from '@/components/ThemeSettings'
 
 export default {
+  watchQuery: true,
   middleware: 'custom-auth',
   components: {
     AppDrawer,
