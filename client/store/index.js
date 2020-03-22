d9d4129fb68a3d02554684d866302e129a21a603
Allan Camilo <acamilo.ag@gmail.com>
2020-03-22 22:52:50 +0200

    set pagination on the state

--- client/store/index.js
+++ client/store/index.js
@@ -1,39 +1,50 @@
-export const state = () => ({
-  drawer: true,
-  loginErrorMsg: null,
-  branch:null,
-  Branch: [],
-  warehouse:null,
-  Warehouse:[]
-})
-
 const logError = error => {
   if (error.response) {
     console.log(error.response.data);
     console.log(error.response.status);
     console.log(error.response.headers);
   }
-}
+};
 
 const removeLocalStoreAuthData = () => {
-  localStorage.removeItem('expiration_date')
-  localStorage.removeItem('token')
-  localStorage.removeItem('user_email')
-  localStorage.removeItem('user_name')
-  localStorage.removeItem('user_id')
-}
+  localStorage.removeItem("expiration_date");
+  localStorage.removeItem("token");
+  localStorage.removeItem("user_email");
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("user_id");
+};
+
+export const state = () => ({
+  drawer: true,
+  loginErrorMsg: null,
+  branch: null,
+  Branch: [],
+  currentSelectedDocument: undefined,
+  warehouse: null,
+  Warehouse: [],
+  productPagination: {}
+});
 
 export const mutations = {
   toggleDrawer(state) {
-    state.drawer = !state.drawer
+    state.drawer = !state.drawer;
+  },
+
+  SET_PRODUCT_PAGINATION(state, pagination) {
+    state.productPagination = pagination;
+  },
+
+  SET_CURRENT_SELECTED_DOCUMENT(state, document) {
+    console.log('mutation to')
+    state.currentSelectedDocument = document;
   },
 
   drawer(state, val) {
-    state.drawer = val
+    state.drawer = val;
   },
 
   storeSignInErrorMsg(state, loginErrorMsg) {
-    state.loginErrorMsg = loginErrorMsg
+    state.loginErrorMsg = loginErrorMsg;
   },
 
   setAuthData(state, user) {
@@ -52,75 +63,91 @@ export const mutations = {
     state.auth.user_name = null;
     state.auth.token_expires_in = null;
     state.auth.user_id = null;
-  },
-
-
-}
+  }
+};
 
 export const actions = {
   async authenticateUser(vuexContext, user) {
-
-    return await this.$axios.$post('/auth/login', {
-      email: user.email,
-      password: user.password
-  })
-  .then(result => {
-    vuexContext.commit("setAuthData", result);
-    localStorage.setItem("token", result.token);
-    localStorage.setItem("expiration_date", result.expiresIn);
-    localStorage.setItem("user_email", result.userEmail);
-    localStorage.setItem("user_name", result.userName);
-    localStorage.setItem("user_id", result.id);
-  })
-  .catch(error => {
-    logError(error);
-    vuexContext.commit('storeSignInErrorMsg', error.response.data);
-  });
+    return await this.$axios
+      .$post("/auth/login", {
+        email: user.email,
+        password: user.password
+      })
+      .then(result => {
+        vuexContext.commit("setAuthData", result);
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("expiration_date", result.expiresIn);
+        localStorage.setItem("user_email", result.userEmail);
+        localStorage.setItem("user_name", result.userName);
+        localStorage.setItem("user_id", result.id);
+      })
+      .catch(error => {
+        logError(error);
+        vuexContext.commit("storeSignInErrorMsg", error.response.data);
+      });
   },
 
-  async getDataAsync(vuexContext, resource) {
-    return await this.$axios
-     .$get(`/${resource}`)
-     .then(response => response)
-     .catch(error => logError(error));
+  setCurrentDocument(vuexContext, document){
+    console.log('Action called')
+    vuexContext.commit('SET_CURRENT_SELECTED_DOCUMENT', document);
+  },
+
+  getDataAsync(vuexContext, resource) {
+    return this.$axios
+      .get(`/${resource}`)
+      .then(response => {
+        if (resource === "products")
+          vuexContext.commit(
+            "SET_PRODUCT_PAGINATION",
+            response.headers["pagination"]
+          );
+
+        return response.data;
+      })
+      .catch(error => logError(error));
   },
 
   async postDataAsync(vuexContext, data) {
     return await this.$axios
-     .$post(`/${data.api_resourse}`, data.post_data)
-     .then(response => response)
-     .catch(error => logError(error));
+      .$post(`/${data.api_resourse}`, data.post_data)
+      .then(response => response)
+      .catch(error => logError(error));
   },
 
   //Auth
-  tryAutoLogin({
-    commit
-  }) {
-    const token = localStorage.getItem('token')
+  tryAutoLogin({ commit }) {
+    const token = localStorage.getItem("token");
     //If the token is not set
-    if (!token) return
+    if (!token) return;
 
-    const expirationDate = new Date(localStorage.getItem('expirationDate'))
-    const now = new Date()
+    const expirationDate = new Date(localStorage.getItem("expirationDate"));
+    const now = new Date();
 
-    if (now > expirationDate) return
-
+    if (now > expirationDate) return;
   },
 
   logout(vuexContext) {
-    vuexContext.commit('clearAuthData')
+    vuexContext.commit("clearAuthData");
     removeLocalStoreAuthData();
   },
 
-  requestPasswordReset(vuexContext) 
-  { 
-    vuexContext.commit('clearAuthData')
+  requestPasswordReset(vuexContext) {
+    vuexContext.commit("clearAuthData");
     removeLocalStoreAuthData();
   }
-} 
+};
 
 export const getters = {
   loginErrorMsg(state) {
     return state.loginErrorMsg;
+  },
+
+  productPagination(state) {
+    return state.productPagination;
+  },
+
+  selectedDocument(state) {
+    return state.currentSelectedDocument;
   }
-}
+  
+};
