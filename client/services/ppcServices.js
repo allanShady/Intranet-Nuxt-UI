451f9b73ee59d1a499f6808e3ce3f642faac46eb
Allan Camilo <acamilo.ag@gmail.com>
2020-03-29 01:10:14 +0200

    Added PPC services

---
+++ client/services/ppcServices.js
@@ -0,0 +1,50 @@
+export default {
+    getTableHeadersView(document) {
+        let headers;
+        if(document === 'DPPC') {
+            //Init headers
+        headers = [
+            { text: "Artigo", value: "product_id" },
+            { text: "descrição", value: "description" },
+            { text: "unidade", value: "unity_id" },
+            { text: "Qtd. Pendente", value: "pending_qtd" },
+            { text: "Qtd. Entrega", value: "quantity" },
+            { text: "Estado", value: "status.description" },
+            { text: "Notas", value: "notes" },
+          ]
+        }
+
+        return headers;
+    },
+
+    prepareLinesToSave(itemsTocope) { 
+        let result = [];
+
+        itemsTocope.forEach(element => {
+            result.push({
+                id: element.id,
+                notes: element.notes,
+                product: element.product_id,
+                unity: element.unity_id,
+                quantity: element.quantity,
+                DocumentId: element.header_id,
+                status_id: element.status.code,
+                factor: 1,
+                branch: localStorage.getItem('branch'), 
+                warehouse: localStorage.getItem('warehouse') || "Sede",
+                location: localStorage.getItem('localization') || "Sede",
+                businessArea: element.business_area
+            });
+        });
+
+        return result;
+    },
+
+    postEvent(apiResource, event) {
+        //return appClient.post(apiResource, event);
+    },
+
+    getEvent(apiResource, id) {
+        //return appClient.get(`${apiResource}/${id}`);
+    }    
+}
