24bee8ef4503d35e9237b2dd1dce65db1000a3f1
Allan Camilo <acamilo.ag@gmail.com>
2020-04-01 09:35:02 +0200

    Added the validation needs

--- client/services/ppcServices.js
+++ client/services/ppcServices.js
@@ -17,14 +17,34 @@ export default {
         return headers;
     },
 
+    getTableHeadersView4Validation(document) {
+        let headers;
+        if(document === 'DPPC') {
+            //Init headers
+        headers = [
+            { text: "Artigo", value: "product_id" },
+            { text: "descrição", value: "description" },
+            { text: "unidade", value: "unity_id" },
+            { text: "Qtd. Pendente", value: "pending_qtd" },
+            { text: "Qtd. Validação", value: "quantity" },
+            { text: "Estado", value: "status.description" },
+            { text: "Notas devolução", value: "delivery_notes" },
+            { text: "Notas validação", value: "validation_notes" },
+          ]
+        }
+
+        return headers;
+    },
+
     prepareLinesToSave(itemsTocope) { 
         let result = [];
-
+        
         itemsTocope.forEach(element => {
             result.push({
                 id: element.id,
                 notes: element.notes,
                 product: element.product_id,
+                description: element.description,
                 unity: element.unity_id,
                 quantity: element.quantity,
                 DocumentId: element.header_id,
@@ -38,6 +58,23 @@ export default {
         });
 
         return result;
+    },
+
+    prepareDeliveryPostData(document_details, doc_type_id)
+    {
+        const post_data = {
+            document_type_id: doc_type_id,
+            date: this.formattDate(new Date()),
+            reference_doc: `Validação de devolução de ${document_details.length} items`,
+        };
+        
+        post_data.details = this.prepareLinesToSave(document_details);
+        return post_data;
+    },
+
+    formattDate(date) {
+        console.log('Date: ', date);
+       return `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
     },
 
     postEvent(apiResource, event) {
