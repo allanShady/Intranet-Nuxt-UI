9463de8868edafea130e084cc32c78ca429d7d8c
Allan Camilo <acamilo.ag@gmail.com>
2020-04-28 22:13:55 +0200

    formatting

--- client/components/inv/Stock.vue
+++ client/components/inv/Stock.vue
@@ -1,17 +1,28 @@
 <template>
-<v-flex>
-  <v-card-title>
-    <v-text-field v-model="search" append-icon="mdi-magnify" label="Pesquisa stocks" single-line hide-details></v-text-field>
-  </v-card-title>
-  <v-data-table :headers="headers" :items="pedding_Items" :search="search">
-    <template v-slot:item.businessArea="{ item }">{{ getPrincipalBussinessArea(item)}}</template>
-  </v-data-table>
+  <v-flex>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Pesquisa stocks"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="pedding_Items"
+      :group-by=" $route.query.tipo == 'gases' ? 'project.description': 'entity'"
+      :search="search"
+    >
+      <template v-slot:item.businessArea="{ item }">{{ getPrincipalBussinessArea(item)}}</template>
+    </v-data-table>
   </v-flex>
 </template>
 
 <script>
-import gasServices from '@/services/gasServices.js';
-import genericServices from '@/services/genericServices.js';
+import gasServices from "@/services/gasServices.js";
+import genericServices from "@/services/genericServices.js";
 
 export default {
   data: () => ({
@@ -20,16 +31,16 @@ export default {
     documentTypes: [],
     pedding_Items: [],
     hasStock: 0,
-    productType: '',
-    url: '',
+    productType: "",
+    url: "",
     classifier: null,
     businessArea: [],
     headers: []
   }),
 
   async created() {
-    //Gets the doc type from query  
-    let doc_type = this.$route.query.tipo; 
+    //Gets the doc type from query
+    let doc_type = this.$route.query.tipo;
 
     //get the products type based on the document
     this.documentTypes = await this.$store.dispatch(
@@ -38,26 +49,24 @@ export default {
     );
 
     //get the products type of the document
-    let product_type = this.documentTypes[0].typeArticle
+    let product_type = this.documentTypes[0].typeArticle;
 
     switch (doc_type) {
-      case 'gases': // --- gas documents
-        this.headers = gasServices.getTableHeadersView('stocks');
+      case "gases": // --- gas documents
+        this.headers = gasServices.getTableHeadersView("stocks");
         this.pedding_Items = await this.getGasBottlesInProject();
-        return; 
-      case 'Equipamentos': // --- Equipments documents
-        this.initStocksBalance('stock_balaces', product_type, '*')
         return;
-      case 'Ferramentas': // --- Tools documents
-        this.initStocksBalance('stock_balaces', product_type, '*')
-        return; 
-      case 'PPC': // --- Tools documents
-        this.initStocksBalance('stock_balaces', product_type, '*')
-        return; 
+      case "Equipamentos": // --- Equipments documents
+        this.initStocksBalance("stock_balaces", product_type, "*");
+        return;
+      case "Ferramentas": // --- Tools documents
+        this.initStocksBalance("stock_balaces", product_type, "*");
+        return;
+      case "PPC": // --- Tools documents
+        this.initStocksBalance("stock_balaces", product_type, "*");
+        return;
       default:
-        this.url = `products/warehouse/${"all"}/filters?hasstock=${1}&type=${
-          product_type
-        }`;
+        this.url = `products/warehouse/${"all"}/filters?hasstock=${1}&type=${product_type}`;
 
         this.headers = [
           { text: "Artigo", value: "Product.id" },
@@ -66,12 +75,15 @@ export default {
           { text: "Filial", value: "Warehouse.branch" },
           { text: "Stock", value: "stock" }
         ];
-        
-        this.pedding_Items = await this.$store.dispatch("getDataAsync", this.url);
+
+        this.pedding_Items = await this.$store.dispatch(
+          "getDataAsync",
+          this.url
+        );
         break;
     }
 
-    console.log('The stock list was created');
+    console.log("The stock list was created");
 
     this.businessArea = await this.$store.dispatch(
       "getDataAsync",
@@ -79,18 +91,19 @@ export default {
     );
 
     if (this.documentTypes[0].isStockMoviment) {
-      
     }
   },
-  methods:{
+  methods: {
     async getGasBottlesInProject(project) {
       return await this.$store.dispatch("getDataAsync", `products/gasbottle`);
     },
 
     async getStockBalances(product_type, entity) {
-      console.log('product_type: ', product_type, 'entity: ', entity)
-      return await this.$store.dispatch("getDataAsync", 
-        `stocks/balance?ProductType=${product_type}&Entity=${entity}`);
+      console.log("product_type: ", product_type, "entity: ", entity);
+      return await this.$store.dispatch(
+        "getDataAsync",
+        `stocks/balance?ProductType=${product_type}&Entity=${entity}`
+      );
     },
 
     async initStocksBalance(tableHeaderView, product_type, entity) {
