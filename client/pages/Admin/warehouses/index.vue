b8044cfa4b4b52e10e578b8562dd71a1864fd262
Allan Camilo <acamilo.ag@gmail.com>
2020-05-07 11:35:58 +0200

    Added the location display area

--- client/pages/Admin/warehouses/index.vue
+++ client/pages/Admin/warehouses/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="page-forms">
     <v-container grid-list-xl fluid>
-      <v-flex lg12 sm12 xs12>
-        <list :list="list"></list>
+      <v-flex>
+        <v-row>
+          <v-col cols="12" sm="12" :md=" locations.records.length ? 8 : 12">
+            <list :list="list" @display-warehouse-locations="updateLocations"></list>
+          </v-col>
+          <v-col md="4" v-show="locations.records.length">
+            <locations-list :list="locations" />
+          </v-col>
+        </v-row>
       </v-flex>
     </v-container>
   </div>
@@ -10,12 +17,14 @@
 
 <script>
 import List from "@/components/admin/List";
+import LocationsList from "@/components/admin/warehouseLocations/list.vue";
 
 export default {
   layout: "dashboard",
   middleware: ["custom-auth"],
   components: {
-    List
+    List,
+    LocationsList
   },
   data() {
     return {
@@ -28,7 +37,7 @@ export default {
           { text: "Morada 1", value: "address1" },
           { text: "Morada 2", value: "address2" },
           { text: "Cidade", value: "city" },
-          { text: "Estado", value: "state" },
+          { text: "Distrito", value: "state" },
           { text: "Pais", value: "country" },
           { text: "", value: "action", sortable: false }
         ],
@@ -36,21 +45,41 @@ export default {
         records: [],
         itemKey: "code",
         loading: false
+      },
+      locations: {
+        title: "Localizaçoes",
+        warehouse: '',
+        headers: [
+          { text: "Localização", value: "code" },
+          { text: "Descrição", value: "description" }
+        ],
+
+        records: [],
+        itemKey: "code"
       }
     };
   },
 
   methods: {
     async initData() {
-      this.list.loading = true
-      this.list.records = await this.$store.dispatch("getDataAsync", "warehouse");
-      this.list.loading = false
+      this.list.loading = true;
+      this.list.records = await this.$store.dispatch(
+        "getDataAsync",
+        "warehouse"
+      );
+
+      this.updateLocations(this.list.records[0]);
+      this.list.loading = false;
+    },
+
+    updateLocations(warehouse){
+      this.locations.records = warehouse.localizations
+      this.locations.warehouse = warehouse.code
     }
   },
 
   created() {
     this.initData();
   }
-  
 };
 </script>
