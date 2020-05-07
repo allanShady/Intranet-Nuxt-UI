92e179df7957e5c4fb56942b72c04e151e7a820c
Allan Camilo <acamilo.ag@gmail.com>
2020-05-07 11:35:05 +0200

    properly create location

--- client/pages/Admin/locations/_id.vue
+++ client/pages/Admin/locations/_id.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-card>
+  <v-card max-width="90%">
     <v-card-title>
-      <span class="headline">{{ $route.params.id == 'create' ? 'Criar localização' : 'Editar localização' }}</span>
+      <span
+        class="headline"
+      >{{ $route.params.id == 'create' ? 'Criar localização' : 'Editar localização' }}</span>
     </v-card-title>
 
     <v-card-text>
       <v-container>
         <v-row>
           <v-col cols="12" sm="12" md="3">
-            <v-text-field v-model="location.code" label="localização"></v-text-field>
+            <v-text-field prepend-icon="mdi mdi-map-marker" v-model="location.code" label="Localização"></v-text-field>
           </v-col>
           <v-col cols="12" sm="12" md="9">
             <v-text-field v-model="location.description" label="Descrição"></v-text-field>
@@ -16,13 +18,22 @@
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" md="3">
-            <v-text-field v-model="location.warehouseId" type="text" label="Armazén"></v-text-field>
+           <v-autocomplete
+              prepend-icon="mdi-warehouse"
+              v-model="location.warehouseId"
+              :items="warehouses"
+              label="Armazén"
+              item-text="description"
+              item-value="code"
+              return-object              
+              :loading="loadingWarehouses"
+            ></v-autocomplete>
           </v-col>
           <v-col md="2">
-            <v-checkbox label="Activo" v-model="location.active" ></v-checkbox>
+            <v-checkbox label="Activo" v-model="location.active"></v-checkbox>
           </v-col>
           <v-col md="2">
-            <v-checkbox label="Anual" v-model="location.anual" ></v-checkbox>
+            <v-checkbox label="Anual" v-model="location.anual"></v-checkbox>
           </v-col>
         </v-row>
       </v-container>
@@ -44,56 +55,67 @@ export default {
       location: { active: true },
       onSave: false,
       isUpdate: false,
+      warehouses: []
     };
   },
 
   async created() {
-
     const locationIdentifier = this.$route.params.id.trim().toLowerCase();
-    if( locationIdentifier !== 'create') 
-      {
-        this.location = await this.$store.dispatch(
-            "getDataAsync",
-            `locations/${locationIdentifier}`
-        );
+    if (locationIdentifier !== "create") {
+      this.location = await this.$store.dispatch(
+        "getDataAsync",
+        `locations/${locationIdentifier}`
+      );
 
-        this.isUpdate = true;
+      this.isUpdate = true;
     }
+
+    this.loadWarehouses();
   },
 
   methods: {
     async save() {
       let post_data = this.location;
+      post_data.warehouseId = post_data.warehouseId.code
 
-     this.onSave = true
+      this.onSave = true;
 
-     if(this.isUpdate);
-     else
-      await this.$store.dispatch("postDataAsync", {
-        api_resourse: "locations",
-        post_data
-      });
+      if (this.isUpdate);
+      else
+        await this.$store.dispatch("postDataAsync", {
+          api_resourse: "locations",
+          post_data
+        });
 
       this.onSave = false;
       this.clearValues();
     },
 
-    clearValues(){
-        this.location.code = null;
-        this.location.description = null;
-        this.location.address1 = null;
-        this.location.address2 = null;
-        this.location.city = null;
-        this.location.state = null;
-        this.location.country = null;
-        this.location.warehouseId = null
-        this.isUpdate = false;
+    async loadWarehouses() {
+      this.loadingWarehouses = true;
+      this.warehouses = await this.$store.dispatch("getDataAsync", "warehouse");
+      this.loadingWarehouses = false;
+    },
+
+    clearValues() {
+      this.location.code = null;
+      this.location.description = null;
+      this.location.address1 = null;
+      this.location.address2 = null;
+      this.location.city = null;
+      this.location.state = null;
+      this.location.country = null;
+      this.location.warehouseId = null;
+      this.isUpdate = false;
     },
 
     close() {
-        const previousPath = this.$route.path.slice(0, this.$route.path.lastIndexOf('/')) 
-        this.$router.push(previousPath)
-    }    
+      const previousPath = this.$route.path.slice(
+        0,
+        this.$route.path.lastIndexOf("/")
+      );
+      this.$router.push(previousPath);
+    }
   }
 };
 </script>
