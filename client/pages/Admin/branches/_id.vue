513a49ea300db960b15541ed8014e950bb663e2e
Allan Camilo <acamilo.ag@gmail.com>
2020-05-08 14:12:14 +0200

    Added the address area

--- client/pages/Admin/branches/_id.vue
+++ client/pages/Admin/branches/_id.vue
@@ -14,25 +14,28 @@
             <v-text-field v-model="branch.description" label="Descrição"></v-text-field>
           </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="12" sm="12" md="6">
-            <v-text-field v-model="branch.address1" type="text" label="Morada 1"></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="12" md="6">
-            <v-text-field v-model="branch.address2" label="Morada 2"></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" sm="12" md="6">
-            <v-text-field v-model="branch.city" type="text" label="Cidade"></v-text-field>
-          </v-col>
-          <v-col>
-            <v-text-field v-model="branch.state" type="text" label="Estado"></v-text-field>
-          </v-col>
-          <v-col>
-            <v-text-field v-model="branch.country" type="text" label="Pais"></v-text-field>
-          </v-col>
-        </v-row>
+        <span>Endereço</span>
+        <v-card outlined class="px-4">
+          <v-row>
+            <v-col cols="12" sm="12" md="6">
+              <v-text-field v-model="branch.address1" type="text" label="Morada 1"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="12" md="6">
+              <v-text-field v-model="branch.address2" label="Morada 2"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="12" md="6">
+              <v-text-field v-model="branch.city" type="text" label="Cidade"></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field v-model="branch.state" type="text" label="Distrito"></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field v-model="branch.country" type="text" label="Pais"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-container>
     </v-card-text>
 
@@ -51,21 +54,19 @@ export default {
     return {
       branch: {},
       onSave: false,
-      isUpdate: false,
+      isUpdate: false
     };
   },
 
   async created() {
-
     const branchIdentifier = this.$route.params.id.trim().toLowerCase();
-    if( branchIdentifier !== 'create') 
-      {
-        this.branch = await this.$store.dispatch(
-            "getDataAsync",
-            `branch/${branchIdentifier}`
-        );
+    if (branchIdentifier !== "create") {
+      this.branch = await this.$store.dispatch(
+        "getDataAsync",
+        `branch/${branchIdentifier}`
+      );
 
-        this.isUpdate = true;
+      if (this.branch) this.isUpdate = true;
     }
   },
 
@@ -73,34 +74,43 @@ export default {
     async save() {
       let post_data = this.branch;
 
-     this.onSave = true
+      this.onSave = true;
 
-     if(this.isUpdate);
-     else
-      await this.$store.dispatch("postDataAsync", {
-        api_resourse: "branch",
-        post_data
-      });
+      if (this.isUpdate) {
+        await this.$store.dispatch("putDataAsync", {
+          api_resourse: "branch",
+          identifier: this.$route.params.id,
+          post_data
+        });
+      } else {
+        await this.$store.dispatch("postDataAsync", {
+          api_resourse: "branch",
+          post_data
+        });
+      }
 
       this.onSave = false;
       this.clearValues();
     },
 
-    clearValues(){
-        this.branch.code = null;
-        this.branch.description = null;
-        this.branch.address1 = null;
-        this.branch.address2 = null;
-        this.branch.city = null;
-        this.branch.state = null;
-        this.branch.country = null;
-        this.isUpdate = false;
+    clearValues() {
+      this.branch.code = null;
+      this.branch.description = null;
+      this.branch.address1 = null;
+      this.branch.address2 = null;
+      this.branch.city = null;
+      this.branch.state = null;
+      this.branch.country = null;
+      this.isUpdate = false;
     },
 
     close() {
-        const previousPath = this.$route.path.slice(0, this.$route.path.lastIndexOf('/')) 
-        this.$router.push(previousPath)
-    }    
+      const previousPath = this.$route.path.slice(
+        0,
+        this.$route.path.lastIndexOf("/")
+      );
+      this.$router.push(previousPath);
+    }
   }
 };
 </script>
