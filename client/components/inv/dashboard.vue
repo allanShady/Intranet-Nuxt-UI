229ba8cfe03c3e7f61fc73402f27d5a820307048
Alan Camilo <acamilo.ag@gmail.com>
2020-06-01 13:18:28 +0200

    Hide the VDPPC doc

--- client/components/inv/dashboard.vue
+++ client/components/inv/dashboard.vue
@@ -8,7 +8,7 @@
               <!--v-col-->
                 <v-btn large outlined class="px-4 mr-4"
                  v-for="document in documentTypes" :key="document.code" color="success"
-                 v-if="!(['ELAV', 'EDPC', 'DDPC', 'ERPC', 'DRPC'].includes(document.code))"  
+                 v-if="!(['ELAV', 'VDPPC', 'EDPC', 'DDPC', 'ERPC', 'DRPC'].includes(document.code))"  
                  @click="openFrm(document)">
                   {{ document.friendlyUserName }}</v-btn>      
                 <v-btn v-if="$route.query.tipo == 'PPC'" large outlined class="px-4 mr-4"  color="warning"  @click="openFrm({code: 'more'})">Mais</v-btn>    
